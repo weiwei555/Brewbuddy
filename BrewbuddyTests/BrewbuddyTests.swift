e5db8795cb3ea708eac1ee49de9b55b1da5f667c
WeiWu <wuwei33@uw.edu>
2025-03-16 15:21:14 -0700

    Initial Commit

---
+++ BrewbuddyTests/BrewbuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrewbuddyTests.swift
+//  BrewbuddyTests
+//
+//  Created by 吴炜 on 3/16/25.
+//
+
+import Testing
+@testable import Brewbuddy
+
+struct BrewbuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BrewbuddyUITests/BrewbuddyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BrewbuddyUITestsLaunchTests.swift
+//  BrewbuddyUITests
+//
+//  Created by 吴炜 on 3/16/25.
+//
+
+import XCTest
+
+final class BrewbuddyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
